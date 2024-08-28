12ba80438d583127209e66bf5c8f1da3e8e1fa71
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2024-08-28 22:32:07 +0300

    Build 1.11.0+openssl-3.3.1

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/samisalkosuo/Libssh2Prebuild/releases/download/1.9.0+OpenSSL_1_1_1h/CSSH-1.9.0+OpenSSL_1_1_1h.xcframework.zip",
-                      checksum: "04582ca1c18cbd6ddc7d73fcb91c9eab8b605cd3b72aa6870fe43f42bb5c42dc")
+                      url: "https://github.com/samisalkosuo/Libssh2Prebuild/releases/download/1.11.0+openssl-3.3.1/CSSH-1.11.0+openssl-3.3.1.xcframework.zip",
+                      checksum: "793ed04f4e3eaa4b96d6a241d7bb49468b2ab1d72feb6231f0de163fe14d28e3")
     ]
 )
